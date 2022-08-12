b728ef174837d10672e43dc993dced6cabfbfad9
Abdul Ahad <ahmmedsumon087@gmai.com>
2022-08-12 09:32:01 +0600

    showing friend details

--- src/components/FriendDetail/FriendDetail.js
+++ src/components/FriendDetail/FriendDetail.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const FriendDetail = () => {
        const {friendId} = useParams();
+
+       const [friend, setFriend] = useState({});
+
+       useEffect(() => {
+              const url = `https://jsonplaceholder.typicode.com/users/${friendId}`;
+              fetch(url)
+              .then(res => res.json())
+              .then(data => setFriend(data)) ;
+
+       }, []);
        return (
               <div>
                      <h2> This is about of my Boundhu....{friendId}</h2>
+
+                     <h2>Name: {friend.name} </h2>
+                     <h3>Username: {friend.username}</h3>
+                     <h4>Email: {friend.email} </h4>
+                     <p><small>Website: {friend.website}</small></p>
+                     <p><small>City: {friend.address?.city} </small></p>
               </div>
        );
 };
